3d5da41106af0d3a03c65756506205ac48303868
Erik Brinkman <ebrink@umich.edu>
2012-12-10 14:27:27 -0500

    Fixed grant_plot

--- grant_plots.py
+++ grant_plots.py
@@ -20,32 +20,42 @@ def makePlot(values, ext, name, xaxis='', yaxis=''):
     print 'Created ' + name + '.pdf'
 
 # CHANGE
-def getStrat(p, pi, n):
-    return StrategicBlockCascadeSolver(p, pi, (n,), ((1,),)).expectedYs()
+def getStrat(p, pi, n, r):
+    m = n-3
+    a = int(round(m*r))
+    b = m-a
+    return StrategicBlockCascadeSolver(p, pi, (1,a,1,b,1),
+        ((0,1,0,0,0),(1,0,1,0,0),(0,1,0,1,0),(0,0,1,0,1),(0,0,0,1,0))
+        ).expectedYs()
 
 # CHANGE
-def getMyop(p, pi, n):
-    return StrategicBlockCascadeSolver(p, pi, (n,), ((1,),), True).expectedYs()
+def getMyop(p, pi, n, r):
+    m = n-3
+    a = int(round(m*r))
+    b = m-a
+    return StrategicBlockCascadeSolver(p, pi, (1,a,1,b,1),
+        ((0,1,0,0,0),(1,0,1,0,0),(0,1,0,1,0),(0,0,1,0,1),(0,0,0,1,0)),
+        True).expectedYs()
 
 getStratV = np.vectorize(getStrat)
 getMyopV  = np.vectorize(getMyop)
 
 pr  = (0.01, 0.5)
 pir = (0.01, 10)
-p = np.linspace(pr[0],   pr[1],  40)
-pi = np.linspace(pir[0], pir[1], 40)
+p = np.linspace(pr[0],   pr[1],  10)
+pi = np.linspace(pir[0], pir[1], 10)
 P, PI = np.meshgrid(p, pi)
 
-ratio = getStratV(P,PI,10) / getMyopV(P,PI,10)
-makePlot(ratio, pr + pir, 'grant_n10', 'p', 'pi')
+ratio = getStratV(P,PI,12,0.5) / getMyopV(P,PI,12,0.5)
+makePlot(ratio, pr + pir, 'grant_n12_r0.5', 'p', 'pi')
 
 nr  = (1,15)
 n  = np.linspace(nr[0],   nr[1],  15)
 pi = np.linspace(pir[0], pir[1], 40)
 N, PI = np.meshgrid(n, pi)
 
-ratio = getStratV(0.25,PI,N) / getMyopV(0.25,PI,N)
-makePlot(ratio, nr + pir, 'grant_p0.25', 'n', 'pi')
+#ratio = getStratV(0.25,PI,N) / getMyopV(0.25,PI,N)
+#makePlot(ratio, nr + pir, 'grant_p0.25', 'n', 'pi')
 
 pr = (0.01, 0.5)
 nr  = (1,15)
@@ -53,5 +63,5 @@ n  = np.linspace(nr[0],   nr[1],  15)
 p = np.linspace(pr[0], pr[1], 20)
 N, P = np.meshgrid(n, p)
 
-ratio = getStratV(P,1.5,N) / getMyopV(P,1.5,N)
-makePlot(ratio, nr + pr, 'grant_pi1.5', 'n', 'p')
+#ratio = getStratV(P,1.5,N) / getMyopV(P,1.5,N)
+#makePlot(ratio, nr + pr, 'grant_pi1.5', 'n', 'p')
